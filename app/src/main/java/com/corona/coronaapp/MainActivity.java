f0f59d2ccf3ef5c5a6ec7e05641268b22bbd5009
leeshble <leeshble@gmail.com>
2021-06-16 15:21:42 +0900

    [FE] style : Card inflate 구현 #1

--- app/src/main/java/com/corona/coronaapp/MainActivity.java
+++ app/src/main/java/com/corona/coronaapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.corona.coronaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh_layout);
+
+        LinearLayout cardListLayout = (LinearLayout)findViewById(R.id.card_list_layout);
+        LayoutInflater cardInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View pressCard = cardInflater.inflate(R.layout.big_card, cardListLayout, true);
+        View guideCard = cardInflater.inflate(R.layout.big_card, cardListLayout, true);
+
+        TextView itemTitleText1 = (TextView)pressCard.findViewById(R.id.item_title_text);
+        itemTitleText1.setText("보도자료");
+
+        TextView itemTitleText2 = (TextView)guideCard.findViewById(R.id.item_title_text);
+        itemTitleText2.setText("지침");
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
